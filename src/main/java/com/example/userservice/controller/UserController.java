da8e0f41aeeb14a9b8ffc996e89505972e652507
Hwangwonuk <hok0173@naver.com>
2023-02-07 21:55:42 +0900

    User Microservice - welcome()메소드

---
+++ src/main/java/com/example/userservice/controller/UserController.java
@@ -0,0 +1,55 @@
+/*
+ * Created by Wonuk Hwang on 2023/02/07
+ * As part of Bigin
+ *
+ * Copyright (C) Bigin (https://bigin.io/main) - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by infra Team <wonuk_hwang@bigin.io>, 2023/02/07
+ */
+package com.example.userservice.controller;
+
+import com.example.userservice.vo.Greeting;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * create on 2023/02/07. create by IntelliJ IDEA.
+ *
+ * <p> </p>
+ * <p> {@link } and {@link } </p> *
+ *
+ * @author wonukHwang
+ * @version 1.0
+ * @see
+ * @since (ex : 5 + 5)
+ */
+
+@RestController
+@RequestMapping("/")
+public class UserController {
+
+  private Environment env;
+
+  @Autowired
+  private Greeting greeting;
+
+  @Autowired
+  public UserController(Environment env) {
+    this.env = env;
+  }
+
+  @GetMapping("/health_check")
+  public String status() {
+    return "It's Working in User Service";
+  }
+
+  @GetMapping("/welcome")
+  public String welcome() {
+//    return env.getProperty("greeting.message");
+    return greeting.getMessage();
+  }
+}
---
+++ src/main/java/com/example/userservice/vo/Greeting.java
@@ -0,0 +1,37 @@
+/*
+ * Created by Wonuk Hwang on 2023/02/07
+ * As part of Bigin
+ *
+ * Copyright (C) Bigin (https://bigin.io/main) - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by infra Team <wonuk_hwang@bigin.io>, 2023/02/07
+ */
+package com.example.userservice.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+/**
+ * create on 2023/02/07. create by IntelliJ IDEA.
+ *
+ * <p> </p>
+ * <p> {@link } and {@link } </p> *
+ *
+ * @author wonukHwang
+ * @version 1.0
+ * @see
+ * @since (ex : 5 + 5)
+ */
+
+@Component
+@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
+public class Greeting {
+  @Value("${greeting.message}")
+  private String message;
+}
